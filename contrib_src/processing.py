3240fce5f7f00fb513ade026299efd0f27267115
Ahmed Hosny <i.ahmedhosny@gmail.com>
2019-03-12 16:09:16 -0400

    adds model source files

---
+++ contrib_src/processing.py
@@ -0,0 +1,40 @@
+import numpy as np
+import os
+import mxnet as mx
+from modelhublib.processor import ImageProcessorBase
+import PIL
+import SimpleITK
+import numpy as np
+import json
+from mxnet.gluon.data.vision import transforms
+
+class ImageProcessor(ImageProcessorBase):
+
+    def _preprocessBeforeConversionToNumpy(self, image):
+        if isinstance(image, PIL.Image.Image):
+            arr = mx.nd.array(image)
+            transform_fn = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ])
+            arr = transform_fn(arr)
+            arr = arr.expand_dims(axis=0)
+            return arr.asnumpy()
+        else:
+            raise IOError("Image Type not supported for preprocessing.")
+
+    def _preprocessAfterConversionToNumpy(self, npArr):
+        return mx.nd.array(npArr)
+
+    def computeOutput(self, inferenceResults):
+        probs = np.squeeze(np.asarray(inferenceResults))
+        with open("model/labels.json") as jsonFile:
+            labels = json.load(jsonFile)
+        result = []
+        for i in range (len(probs)):
+            obj = {'label': str(labels[str(i)]),
+                    'probability': float(probs[i])}
+            result.append(obj)
+        return result
